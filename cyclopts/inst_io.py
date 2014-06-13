84917a148e686a9b3ca5f823c7807e1a67c0e08a
Matthew Gidden <matthew.gidden@gmail.com>
2014-06-13 13:21:22 -0500

    group reading and writing complete

--- cyclopts/inst_io.py
+++ cyclopts/inst_io.py
@@ -5,6 +5,7 @@ to and from HDF5 databases.
 """
 import numpy as np
 import tables as t
+from collections import Iterable
 
 from cyclopts.instance import ExGroup, ExNode, ExArc 
 
@@ -16,6 +17,25 @@ _tbl_names = {
     }
 _filters = t.Filters(complevel=4)
 
+# this must be kept up to date with the cyclopts.instance classes
+_dtypes = {
+    "ExGroup": np.dtype([
+        ("instid", ('str', 16)), # 16 bytes for uuid
+        ("id", np.int64),
+        ("kind", np.bool_),
+        ("caps", (np.float64, _N_CAPS_MAX),), # array of size N_CAPS_MAX
+        ("qty", np.float64),
+        ]),
+    "ExNode": {
+        "instit": np.dtype((np.void, 16)), # 16 bytes for uuid
+        "id": np.int64,
+        },
+    "ExArc": {
+        "instit": np.dtype((np.void, 16)), # 16 bytes for uuid
+        "id": np.int64,
+        },
+    }
+
 """returns a list of object members that do not start with '_' or '__'. This is
 specifically useful for listing the variables of an xdressed object.
 """
@@ -24,32 +44,44 @@ def xdvars(obj):
                 if not callable(attr) \
                 and not attr.startswith('__') and not attr.startswith('_')]
 
+"""Checks if a file has known Exchange-related tables and adds them if not"""
+def check_extables(h5file, h5node):
+    names = {"ExGroup": "ExchangeGroups"}
+    for obj, name in names.items():#_tbl_names.items():
+        if not name in h5node._v_children:
+            h5file.create_table(h5node, name, _dtypes[obj], filters=_filters)
+
 def write_exgroups(h5node, instid, groups):
     name = _tbl_names["ExGroup"]
-    if not name in h5node._v_children:
-        # this must be kept up to date with the cyclopts.instance classes
-        dtype = {
-            "id": np.intc,
-            "kind": np.bool_,
-            "caps": np.dtype((np.float64, (_N_CAPS_MAX,))), # array of size N_CAPS_MAX
-            "qty": np.float64,
-            }
-        h5file.create_table(h5node, name, dtype, filters=_filters)
-    row = h5node.name.row
+    tbl = getattr(h5node, name)
+    row = tbl.row
     for g in groups:
-        row['instid'] = instid
+        row['instid'] = instid.bytes
         for var in xdvars(g):
-            row[var] = getattr(g, var)
+            attr = getattr(g, var)
+            if isinstance(attr, Iterable):
+                zeros = [0] * (_N_CAPS_MAX - len(attr))
+                attr = np.append(attr, zeros) # up the array to len 10 with 0 values
+            row[var] = attr
         row.append()
+    tbl.flush()
 
 def read_exgroups(h5node, instid):
     name = _tbl_names["ExGroup"]
     groups = []
-    rows = h5node.name.where("""instid == instid""")
+    tbl = getattr(h5node, name)
+    byteid = instid.bytes
+    rows = tbl.where('instid == byteid')
     for row in rows:
         g = ExGroup()
         for var in xdvars(g):
-            setattr(g, var, row[var])
+            attr = getattr(g, var)
+            if isinstance(attr, Iterable):
+                ary = row[var]
+                attr = ary[ary > 0] 
+            else:
+                attr = row[var]
+            setattr(g, var, attr)
         groups.append(g)
     return groups
 
--- tests/test_inst_io.py
+++ tests/test_inst_io.py
@@ -7,7 +7,10 @@ import numpy as np
 from numpy.testing import assert_array_equal
 import nose
 from nose.tools import assert_equal
-from collections import Sequence
+from collections import Iterable
+import uuid
+import tables as t
+import os
 
 # equality comparison is required on the python level because xdress can't
 # currently wrap dunder magic, see 
@@ -17,15 +20,26 @@ def assert_xd_equal(exp, obs):
     for var in xdvars(exp):
         vexp = getattr(exp, var)
         vobs = getattr(obs, var)
-        if isinstance(exp, Sequence):
+        if isinstance(vexp, Iterable):
             assert_array_equal(vexp, vobs)
         else:
             assert_equal(vexp, vobs)
 
 def test_exgroup():
-    exp = ExGroup(1, False, np.array([1], dtype='float'), 3)
-    obs = ExGroup(1, False, np.array([1], dtype='float'), 3)
-    assert_xd_equal(exp, obs)
+    instid = uuid.uuid4()
+    tmp = "tmp_{0}".format(instid)
+    h5file = t.open_file(tmp, mode='w',)
+    h5node = h5file.root
+    iio.check_extables(h5file, h5node)
+    exp = [ExGroup(1, True, np.array([1], dtype='float'), 3), 
+           ExGroup(6, False, np.array([2, 3.5], dtype='float'))]
+    iio.write_exgroups(h5node, instid, exp)
+    obs = iio.read_exgroups(h5node, instid)
+    assert_equal(len(exp), len(obs))
+    for i in range(len(exp)):
+        assert_xd_equal(exp[i], obs[i])
+    h5file.close()
+    os.remove(tmp)
 
 def test_exnode():
     exp = ExNode(1, 2, False, 3)
