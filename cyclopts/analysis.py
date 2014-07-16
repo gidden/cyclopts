ad3a36fdc1452be908f20cae4507b42e6adb529e
Matthew Gidden <matthew.gidden@gmail.com>
2014-07-16 13:35:26 -0500

    added a graph of req qtys

--- cyclopts/analysis.py
+++ cyclopts/analysis.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.lines as lines
+import sys
 
 def imarkers():
     return iter(['x', 's', 'o', 'v', '^', '+'])
@@ -35,10 +36,13 @@ def plot_xy(props, xhandle, yvals):
               bbox_to_anchor=(1, 0.5))
     return ax
 
-def plot_xyz(xtbl, xhandle, ytbl, yhandle, zvals):
+def plot_xyz(xtbl, xhandle, ytbl, yhandle, zvals, toinst=None, fromkey=None):
     xvals = {x['instid']: x[xhandle] for x in xtbl.iterrows()}
-    yvals = {x['instid']: x[yhandle] for x in ytbl.iterrows()}
-   
+    if toinst is None:
+        yvals = {x['instid']: x[yhandle] for x in ytbl.iterrows()}
+    else:
+        yvals = {toinst[x[fromkey]]: x[yhandle] for x in ytbl.iterrows()}
+    
     m_it = imarkers()
     c_it = icolors()
 
@@ -47,18 +51,27 @@ def plot_xyz(xtbl, xhandle, ytbl, yhandle, zvals):
     plts = {}
     proxy = {}
     solvers = sorted(zvals.keys())
+    minx, maxx = sys.float_info.max, sys.float_info.min
+    miny, maxy = sys.float_info.max, sys.float_info.min
+    minz, maxz = sys.float_info.max, sys.float_info.min
     for s in solvers:
         l = zvals[s]
         x = [xvals[i[0]] for i in l]
+        minx = min(x) if min(x) < minx else minx
+        maxx = max(x) if max(x) > maxx else maxx
         y = [yvals[i[0]] for i in l]
+        miny = min(y) if min(y) < miny else miny
+        maxy = max(y) if max(y) > maxy else maxy
         z = [i[1] for i in l]
+        minz = min(z) if min(z) < minz else minz
+        maxz = max(z) if max(z) > maxz else maxz
         c = c_it.next()
         m = m_it.next()
         plts[s] = ax.scatter(x, y, z, c=c, marker=m, label=s)  
         proxy[s] = lines.Line2D([0],[0], linestyle="none", c=c, marker=m)
-    ax.set_xlim(0)
-    ax.set_ylim(0)
-    ax.set_zlim(0)
+    ax.set_xlim(minx, maxx)
+    ax.set_ylim(miny, maxy)
+    ax.set_zlim(minz, maxz)
 
     # Put a legend to the right of the current axis
     box = ax.get_position()
