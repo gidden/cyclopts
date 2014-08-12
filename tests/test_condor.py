3bcbc8f4287233934e25eb1517d632291051c166
Matthew Gidden <matthew.gidden@gmail.com>
2014-08-12 17:06:42 -0500

    all tests fixed except combine test

--- tests/test_condor.py
+++ tests/test_condor.py
@@ -6,6 +6,7 @@ import shutil
 import paramiko as pm
 import warnings
 import tarfile
+import tables as t
 
 from cyclopts.condor import dag
 from cyclopts.condor import queue
@@ -13,12 +14,36 @@ from cyclopts.condor import utils
 from cyclopts import main
 from cyclopts import tools
 
-from test_main import exp_uuid_arcs 
-
 import nose
 from nose.tools import assert_equal, assert_true
 
 from utils import timeout, TimeoutError
+
+"""this is a print out of uid hexs and their number of arcs taken from
+cyclopts/tests/files/exp_instances.h5 on 6/15/14 via
+
+.. code-block::
+
+  import tables as t
+  h5f = t.open_file('cyclopts/tests/files/exp_instances.h5', 'r')
+  h5n = h5f.root.Instances.ExchangeInstProperties
+  for row in h5n.iterrows():
+    print(uuid.UUID(bytes=row['instid']).hex, row['n_arcs'])
+
+"""
+_exp_uuid_arcs = []
+def exp_uuid_arcs():
+    global _exp_uuid_arcs    
+    if len(_exp_uuid_arcs) == 0:
+        base = os.path.dirname(os.path.abspath(__file__))
+        pth = os.path.join(base, 'files', 'obs_valid_in.h5')
+        h5file = t.open_file(pth, 'r')
+        path = '/Family/ResourceExchange/ExchangeInstProperties'
+        tbl = h5file.get_node(path)
+        _exp_uuid_arcs = [(uuid.UUID(bytes=row['instid']).hex, row['n_arcs']) \
+                              for row in tbl.iterrows()]
+        h5file.close()
+    return _exp_uuid_arcs
 
 def test_gen_dag_tar():
     base = os.path.dirname(os.path.abspath(__file__))
@@ -95,11 +120,16 @@ def test_get_files():
     tstfiles = [prefix + 'test_file', prefix + 'other_file']
     touchline = " ".join("/".join([remotedir, f]) for f in tstfiles)
     cmd = "mkdir -p {0} && touch {1}".format(remotedir, touchline)
-    client.connect(host, username=user, key_filename=keyfile)
-    stdin, stdout, stderr = utils.exec_remote_cmd(client, cmd, verbose=True)
-    print("getting", remotedir)
-    nfiles = utils.get_files(client, remotedir, localdir, prefix + '*')
-    client.close()
+    
+    try:
+        client.connect(host, username=user, key_filename=keyfile)
+        stdin, stdout, stderr = utils.exec_remote_cmd(client, cmd, verbose=True)
+        print("getting", remotedir)
+        nfiles = utils.get_files(client, remotedir, localdir, prefix + '*')
+        client.close()
+    except TimeoutError:
+        warnings.warn('could not connect via ssh to {0}@{1}'.format(user, host))
+        return
     
     assert_equal(set(os.listdir(localdir)), set(tstfiles))
     
