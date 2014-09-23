b1f309880409d091d70ddd818fbe5bcef8fc8b5b
Matthew Gidden <matthew.gidden@gmail.com>
2014-09-23 17:20:35 -0500

    added color mapping support based on parameters

--- cyclopts/analysis.py
+++ cyclopts/analysis.py
@@ -3,6 +3,7 @@ from matplotlib.font_manager import FontProperties
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.lines as lines
 import sys
+from collections import defaultdict
 
 def imarkers():
     return iter(['x', 's', 'o', 'v', '^', '+'])
@@ -55,6 +56,55 @@ def grab_data(h5file, path, col, matching=None):
         scol, search = matching
         data = {x['instid']: x[col] for x in h5node.iterrows() if x[scol] in search}
     return data
+
+def param_mapping(h5file, path, kcol, vcol):
+    """return a mapping of params to all values found
+    
+    Parameters
+    ----------
+    h5file : PyTables HDF5 File handle
+    path : str
+        the path to the appropriate table
+    kcol : str
+        the key column name
+    vcol : str
+        the value column name
+    
+    Return
+    ------
+    mapping : dict
+        a mapping from key columns to a set of all found value columns
+    """
+    h5node = h5file.get_node(path)
+    data = defaultdict(set)
+    for x in h5node.iterrows():
+        data[x[kcol]].add(x[vcol])
+    return data
+
+def param_to_iids(h5file, fam_path, sp_path, col):
+    """Return a mapping of parameter values to instids
+    
+    Parameters
+    ----------
+    h5file : PyTables HDF5 File handle
+    fam_path : str
+        the path to the appropriate family table (for param ids to inst ids)
+    sp_path : str
+        the path to the appropriate species table (for param to param ids)
+    col : str
+        the parameter column name
+
+    Return
+    ------
+    mapping : dict
+        a mapping from key columns to a set of all found value columns
+    """
+    pid_to_iids = param_mapping(h5file, fam_path, 'paramid', 'instid')
+    ret = defaultdict(set)
+    for p, pids in param_mapping(h5file, sp_path, col, 'paramid').items():
+        for pid in pids:
+            ret[p].update(pid_to_iids[pid])
+    return ret
 
 def multi_scatter(x, ys, ax=None):
     """returns a plot object with multiple y values
